3c4fe356e8cd1c467bc20e5a142a97741199ad60
zakmos <z.zza@hotmail.com>
2021-12-18 13:45:50 +0100

    added not found page

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+export default class NotFound extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                    </div>
+                    <div className="col-6">
+                    <h1 className="alert alert-danger" role="alert">“404: Seite nicht gefunden”?</h1>
+                    </div>
+                    <div className="col">
+                    </div>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+            </div>
+        )
+    }
+}
