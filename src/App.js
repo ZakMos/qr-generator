b317acd9a3436b67586461e9bf0842a5da92ddb6
zakmos <z.zza@hotmail.com>
2021-12-18 13:46:06 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css'
 import Data from './views/Data';
-import Header from './components/Header';
-import Footer from './components/Footer';
 
 const App = () => {
  
   return (
-    <>
-    <div className="App">
-      <Header />
-        <Data />
-      <Footer />
-    </div>
-    </>
+       <Data />
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
 import App from './App';
-import './App.css';
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Impressum from './views/Impressum';
+import NotFound from './components/NotFound'
 
+import './App.css';
+
+const routing =(
+    <Router>
+      <div className="App">
+        <Header />
+          <Routes>
+              <Route path="/" element={<App/>}></Route>
+              <Route path="impressum" element={<Impressum/>}></Route>
+              <Route path="/*" element={<NotFound/>}></Route>
+
+          </Routes>
+        <Footer />
+      </div>
+    </Router>
+);
+  
+ReactDOM.render(routing, document.getElementById('root'));
