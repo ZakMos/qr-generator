ebdf30e6a1b3ea0282829f3035f99220d944f1bd
zakmos <z.zza@hotmail.com>
2021-12-18 13:45:35 +0100

    addeed impressum page

---
+++ src/views/Impressum.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+export default class Impressum extends Component {
+    render() {
+        return (
+            <>
+            <div className="container">
+                <h1 className="center">Impressum</h1>
+                <div className="card">
+                    <div className="card-body">
+                        <div >
+                            <h3>Angaben gemäß § 5 TMG:</h3>
+                            <p>MVZ Dr. Hijazi GmbH</p>
+                            <p>Zahnzentrum Dr. Hijazi</p>
+                            <p>Berliner Straße 25</p>
+                            <p>13507 Berlin- Tegel</p>
+                        </div>
+                        <p>gesetzliche Berufsbezeichnung:</p>
+                        <p>Zahnarzt, Fachzahnarzt für Oralchirurgie und Master of Oralmedizin in Implantologie</p>
+                        <p>(Die Titel wurde in der Bundesrepublik Deutschland verliehen)</p>
+                        <p>Zuständige Kammer: Zahnärztekammer Berlin</p>
+                        <p>Verliehen durch: Bundesrepublik Deutschland</p>
+                        <p>Es gelten folgende berufsrechtliche Regelungen: Berufsrechtliche Regelungen der Zahnärztekammer Berlin</p>
+                        <p>Regelungen einsehbar unter: http://www.zaek-berlin.de/zahnaerzte/berufsrecht.html</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <h4>Kontakt:</h4>
+                        <p>Tel.: 030/ 43748465</p>
+                        <p>Fax: 030/ 43749257</p>
+                        <p>E-Mail: info@zahnzentrumdrhijazi.de</p>
+                        <p>Umsatzsteuer-Identifikationsnu</p>
+                        <p>Uts-Id-Nummer: DE318878992</p>
+                        <p>HRB 194448 B</p>
+                        <p>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</p>
+                        <p>Dr. Mohammed Hijazi, Berlinerstr. 25, 13507 Berlin</p>
+                        <p>lnformation zur außergerichtlichen Streitbeilegung:</p>
+                        <p>lm Hinblick auf eine Verpflichtung aus §§ 36, 37 VSBG setzen wir Sie davon in Kenntnis, dass unsere Praxis zu einem außergerichtlichen Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle i. S. d. VSBG nicht verpflichtet ist und an einem solchen Verfahren nicht teilnimmt.</p>
+                        <p>Die Zahnärztekammer Berlin hat eine Schlichtungsstelle für Streitigkeiten aus dem Berufsverhältnis nach dem Berliner Kammergesetz (BKG) eingerichtet, die von Patientinnen und Patienten bei Zweifeln an der Richtigkeit zahnärztlicher Leistungen angerufen werden kann.</p>
+                    </div>
+                </div>
+            </div>
+            </>
+        )
+    }
+}
